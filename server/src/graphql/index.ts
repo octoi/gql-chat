e6cb6ff34e2a954b944b5ab02fb3501710ed0491
fadhilsaheer <fadhilsaheer@gmail.com>
2021-12-27 08:56:33 +0530

    schema

--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -1,8 +1,10 @@
 import { GraphQLSchema } from 'graphql';
 import { Mutations } from './mutations';
 import { Queries } from './queries';
+import { Subscription } from './subscription';
 
 export const schema = new GraphQLSchema({
   query: Queries,
   mutation: Mutations,
+  subscription: Subscription,
 });
